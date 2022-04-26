8ffc92544e3a849fe3725263a64238c987b8cfb3
geradramirez1629 <gerardo.ramirez2908@gmail.com>
2022-04-26 10:26:27 -0400

    main

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ import ipfsLogoText from '../../media/logos/ipfs-text.svg'
 const Header = ({ t }) => (
   <div className='flex items-center pa4' style={{ height: '150px' }}>
     <a href='/'>
-      <img src={ipfsLogoText} style={{ height: 65 }} alt='IPFS' />
+      <img src={ipfsLogoText} style={{ height: 65 }} alt='Cybermint IPFS' />
     </a>
     <div className='h3 ba mh2 aqua' />
     <div className='ml2 pb2 f2 fw1 aqua montserrat'>{t('header')}</div>
